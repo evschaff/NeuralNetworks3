7bebbc4e7f72311ca14945b721fb97c54c26f0b9
Evan Schaffer <evschaff@microsoft.com>
2023-02-24 12:53:58 -0600

    one more

--- SimpleFunction/Program.cs
+++ SimpleFunction/Program.cs
@@ -14,6 +14,7 @@ namespace SimpleFunction
             {
                 // This is a source link test
                 // Second test
+                // Third test
 
                 var inputs = GetInputs(image);
 
